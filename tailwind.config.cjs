a612c543fe96670565535652ca38e4eb07465c3f
Giulio Zanchetta <zank@outlook.it>
2023-08-09 22:57:11 +0200

    Add prettier-plugin-astro to tailwind.config.cjs plugins
    This commit adds the prettier-plugin-astro to the list of plugins in the tailwind.config.cjs file. This plugin is required for formatting Astro code using Prettier.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,6 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [require("@tailwindcss/typography"), require.resolve("prettier-plugin-astro")],
 	darkMode: "class",
 };
