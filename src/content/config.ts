310e059b9aa698b15cfb6ca90add4cfa78cd1df5
Giulio Zanchetta <zank@outlook.it>
2023-08-11 21:27:29 +0200

    Remove team members Janette Lynch, Marcell Ziemann, and Robert Palmer
    - Remove `teamCollection` from `config.ts`
    - Delete files: janette-lynch.md, marcell-ziemann.md, robert-palmer.md

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,22 +18,8 @@ const blogCollection = defineCollection({
 	}),
 });
 
-const teamCollection = defineCollection({
-	schema: z.object({
-		draft: z.boolean(),
-		name: z.string(),
-		title: z.string(),
-		avatar: z.object({
-			src: z.string(),
-			alt: z.string(),
-		}),
-		publishDate: z.string().transform((str) => new Date(str)),
-	}),
-});
-
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
 	blog: blogCollection,
-	team: teamCollection,
 };
