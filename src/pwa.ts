2755a7bd3aa73af4e9a2eba9b4e574c1da43a240
Giulio Zanchetta <zank@outlook.it>
2023-08-11 23:08:51 +0200

    Refactor blog category and tag paths
    - Refactored the paths in the `change-language-in-dynamic-pages.js` file to use updated path patterns for blog categories and tags.
    - Updated the `getStaticPaths` function in the `[slug].astro`, `[category]/[...page].astro`, and `[tag]/[...page].astro` files to use the new path patterns for localized posts.
    - Renamed the `[category]/[...page].astro` file to `category/[category].astro`.
    - Renamed the `[tag]/[...page].astro` file to `tag/[tag].astro`.
    - Updated links in the `[slug].astro`, `category/[category].astro`, and `tag/[tag].astro` files to reflect changes in path patterns.

--- src/pwa.ts
+++ src/pwa.ts
@@ -4,10 +4,10 @@ registerSW({
 	immediate: true,
 	onRegisteredSW(swScriptUrl) {
 		// eslint-disable-next-line no-console
-		console.log("SW registered: ", swScriptUrl);
+		// console.log("SW registered: ", swScriptUrl);
 	},
 	onOfflineReady() {
 		// eslint-disable-next-line no-console
-		console.log("PWA application ready to work offline");
+		// console.log("PWA application ready to work offline");
 	},
 });
